9e95d6af96eeafdb6768da67075517a2ccc2ae91
rookmarkjoy <onevisionjoy@gmail.com>
2020-11-01 16:15:49 +0530

    wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'databasedemo' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'usernamedemo' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'password_here' );
